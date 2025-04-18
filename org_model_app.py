647dbac2e2a5f967a75a9ef4e507317d1bbaa004
Joe Bell <joe@hungryjoe.tv>
2025-04-18 19:53:15 +0100

    Update org_model_app.py

--- org_model_app.py
+++ org_model_app.py
@@ -33,7 +33,7 @@ import graphviz
 st.subheader("📈 Org Chart Preview")
 
 # Generate Graphviz dot code
-dot_string = "digraph G {\nlayout=neato;\n"
+dot = graphviz.Digraph()
 
 # Add boss node
 dot.node("Boss", "👑 Boss")
