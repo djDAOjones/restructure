33095f7bd9b783ed2407b9b13b6798149837579a
djDAOjones <joe@hungryjoe.tv>
2025-04-20 02:50:16 +0100

    r

--- org_model_app.py
+++ org_model_app.py
@@ -41,7 +41,7 @@ seniority_input = st.slider("Seniority afforded", 70, 100, 100, format="%d%%")  
 seniority = (seniority_input - 76) * (100 / (100 - 76))
 
 workers_per_mgr = st.slider("Workers per FSS manager", 5, 10, 10)
-show_content_as_team = st.checkbox("Show Learning Content as Separate Team", value=True)
+show_content_as_team = st.checkbox("Show Learning Content as Separate Team", value=False)
 
 chart_container = st.container()
 
